f5b2fcd999488311eb713a3fbd20cdea5e850ced
rickonz <49249770+rickonz@users.noreply.github.com>
2021-10-26 01:36:57 -0700

    update assignment 1

--- assignment01.py
+++ assignment01.py
@@ -10,61 +10,7 @@
 
 # 2718281828459045235360287471352662497757247093699959574966967627
 
-import sys
- 
-# the setrecursionlimit function is
-# used to modify the default recursion
-# limit set by python. Using this,
-# we can increase the recursion limit
-# to satisfy our needs
- 
-sys.setrecursionlimit(10**6)
+# THE KARATSUBA ALGORITHM (Karatsuba multiplication)
 
-# def karastuba(x, y):
-#     if x<10 or y <10:
-#         return x*y
-#     else:
-#         n = max(len(str(x)), len(str(y)))
-#         half = n/2
-
-#         a = x / 10**(half)
-#         b = x % 10**(half)
-#         c = y / 10**half
-#         d = y % 10**half
-
-#         ac = karastuba(a,c)
-#         bd = karastuba(b,d)
-#         ad = karastuba(a,d)
-#         bc = karastuba(b,c)
-
-#         prod = (10**2*half)*ac + (10**half)*(ad+bc) + bd
-
-#         return prod
-
-def karastuba(x,y):
-    if len(str(x)) == 1 or len(str(y)) == 1:
-        return x*y
-    else:
-        n = max(len(str(x)),len(str(y)))
-        nby2 = n / 2
-        a = x / 10**(nby2)
-        b = x % 10**(nby2)
-        c = y / 10**(nby2)
-        d = y % 10**(nby2)
-        ac = karastuba(a,c)
-        bd = karastuba(b,d)
-        ad_plus_bc = karastuba(a+b,c+d) - ac - bd
-        # this little trick, writing n as 2*nby2 takes care of both even and odd n
-        prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
-        return prod
-
-x = 3141592653589793238462643383279502884197169399375105820974944592
-y = 2718281828459045235360287471352662497757247093699959574966967627
-print(karastuba(x, y))
-
-
-
-
-
-
+# Python Code
 
